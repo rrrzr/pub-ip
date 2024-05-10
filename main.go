72b4484ae1395e51beafa76ecfd3ece77d6166fd
eessn <folder@163.com>
2024-05-10 11:10:19 +0800

    Update main.go

--- main.go
+++ main.go
@@ -12,8 +12,8 @@ func main() {
 	http.HandleFunc("/", handleRequest)
 
 	// 设置监听的端口
-	fmt.Println("Server is listening on port 8080...")
-	http.ListenAndServe(":8080", nil)
+	fmt.Println("已启动服务监听端口8001")
+	http.ListenAndServe(":8001", nil)
 }
 
 // handleRequest 处理入站的 HTTP 请求
@@ -46,11 +46,11 @@ func getIPAddress(r *http.Request) string {
 	// 尝试从 X-Forwarded-For 头部获取 IP 地址
 	xForwardedFor := r.Header.Get("X-Forwarded-For")
 	if xForwardedFor != "" {
-		// 取 X-Forwarded-For 中的第一个 IP（可能有多个 IP，由代理添加）
+		// 取 X-Forwarded-For 中的第一个 IP (可能有多个 IP，由代理添加)
 		return strings.Split(xForwardedFor, ",")[0]
 	}
 
-	// 否则，直接从远程地址获取 IP（并去除可能附加的端口信息）
+	// 否则，直接从远程地址获取 IP (并去除可能附加的端口信息)
 	ip, _, _ := net.SplitHostPort(r.RemoteAddr)
 	return ip
 }
